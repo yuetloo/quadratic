28be9eb3ab78ea137f37124ad817b3a9843d3fc3
Yuet Loo Wong <contact@yuetloo.com>
2021-05-30 12:58:10 -0400

    fix heroku error

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 module.exports = router;
